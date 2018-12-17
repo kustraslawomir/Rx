ec9fa0bddca5a81117bf3ef851bd9668b7aae675
skustra <kustraslawomir@gmail.com>
2018-12-17 19:12:20 +0100

    pure functions definition

--- app/src/main/java/slawomir/kustra/rx/SecondChapter.kt
+++ app/src/main/java/slawomir/kustra/rx/SecondChapter.kt
@@ -12,9 +12,21 @@ class SecondChapter : AppCompatActivity() {
         setContentView(R.layout.activity_second_chapter)
 
         val sum = { x: Int, y: Int -> x + y }
-        println("sum 4+5 = ${sum(4, 5)}")
+        println("sum 4+5: ${sum(4, 5)}")
 
         val multipleRandom = { x: Int -> (Random.nextInt(15) + 1) * x }
-        println("multipleRandom 3 = ${multipleRandom(2)}")
+        println("multipleRandom 3: ${multipleRandom(2)}")
+
+        println("square from 5: ${square(5)}")
     }
+
+    /*
+    Pure functions definition:
+    if the return value of a function is completely dependent on its arguments/parameters,
+    then this function may be referred to as a pure function
+     */
+
+    private fun square(n: Int) = n * n
+
+
 }
