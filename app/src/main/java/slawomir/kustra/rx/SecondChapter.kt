efa54883225248b3f85904a4130b7998dfd519a3
skustra <kustraslawomir@gmail.com>
2018-12-17 19:07:22 +0100

    lamda expression

--- app/src/main/java/slawomir/kustra/rx/SecondChapter.kt
+++ app/src/main/java/slawomir/kustra/rx/SecondChapter.kt
@@ -2,11 +2,15 @@ package slawomir.kustra.rx
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class SecondChapter : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_chapter)
+
+        val sum = { x: Int, y: Int -> x + y }
+        println("sum 4+5 = ${sum(4, 5)}")
     }
 }
