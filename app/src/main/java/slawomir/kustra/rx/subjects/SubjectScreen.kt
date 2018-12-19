2e00dda1ca99e27e7d3abb2de680192437925da3
slawomir.kustra <slawomir.kustra@witchcraftstudios.com>
2018-12-19 11:21:12 +0100

    publish subject

--- app/src/main/java/slawomir/kustra/rx/subjects/SubjectScreen.kt
+++ app/src/main/java/slawomir/kustra/rx/subjects/SubjectScreen.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.subscriptions.ArrayCompositeSubscription
+import io.reactivex.subjects.PublishSubject
 import io.reactivex.subjects.ReplaySubject
 import slawomir.kustra.rx.R
 
@@ -22,6 +23,38 @@ class SubjectScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_subject_screen)
+        /*
+        ----------  PUBLISH SUBJECT ----------
+       Publish subject emits items only after subject is subscribed to observer.
+       */
+        val publishSubject: PublishSubject<String> = PublishSubject.create()
+        publishSubject.onNext("1")
+        publishSubject.onNext("2")
+        publishSubject.subscribe(object : Observer<String> {
+            override fun onComplete() {
+                println("publishSubject onComplete")
+            }
+
+            override fun onSubscribe(disposable: Disposable) {
+                println("publishSubject onSubscribe")
+            }
+
+            override fun onNext(value: String) {
+                println("publishSubject onNext: $value")
+            }
+
+            override fun onError(e: Throwable) {
+                println("publishSubject onError: ${e.message}")
+            }
+        })
+        publishSubject.onNext("3")
+        publishSubject.onComplete()
+
+        /*
+      ----------  REPLAY SUBJECT ----------
+       Even if on next called is before replay subject subscribe(), items will be emitted and
+       invoked in onNext(cat: Cat) method.
+        */
 
         val replaySubject: ReplaySubject<Cat> = ReplaySubject.create()
 
@@ -29,25 +62,21 @@ class SubjectScreen : AppCompatActivity() {
         replaySubject.onNext(Cat("Bonifacy", false))
         replaySubject.onNext(Cat("Klakier", true))
 
-        /*
-        Even if on next called is before replay subject subscribe(), items will be emitted and
-        invoked in onNext(cat: Cat) method.
-         */
         replaySubject.subscribe(object : Observer<Cat> {
             override fun onComplete() {
-            println("on complete replay subject")
+                println("replaySubject on complete")
             }
 
             override fun onSubscribe(disposable: Disposable) {
-                println("onSubscribe replay subject")
+                println("replaySubject onSubscribe")
             }
 
             override fun onNext(cat: Cat) {
-                println("replay cat: $cat")
+                println("replaySubject cat: $cat")
             }
 
             override fun onError(throwable: Throwable) {
-                println("replay error: ${throwable.message}")
+                println("replaySubject error: ${throwable.message}")
             }
         })
 
